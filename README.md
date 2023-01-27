1ef79a9fb214c88bf7cdef170fd30e3de569e68e
Victoria Danchenko <vika.sonne@rambler.ru>
2023-01-28 00:20:21 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,149 @@
 # special-purposes-keyboard
+**:snake: Python 3 CLI utility to programming special purposes keyboard**
+_____________________________________________________
+
+## This is an multi-OS implementation of programming a special purposes keyboard:
+![](/media/640x640.jpg_.webp)
+
+## Usage scenario
+
+- Edit layout file.
+- Program special purposes keyboard by this utility with command-line interface.
+
+## Advantages of this utility
+- You may have several layout files.
+- Not need to use GUI to program and even for edit layout file (Unix way :robot:).
+- Multy-OS usage. Feel free. :cowboy_hat_face:
+
+# Layout file
+
+Key programming pattern:
+
+`key=symbol`
+
+  where:
+
+`key` - special purposes keyboard key;
+
+`symbol` - action to do.
+
+ ### Symbols examples:
+ ```
+ multi-key sequence: a,b,c
+ with special keys: ctrl+shift+alt+meta+a
+ the same: c+s+a+m+a
+ multi-key sequence: c+s+a+m+a,b,c
+```
+ For knob programming use section with name `Kn` for knob `n`. Example:
+ ```
+[K1]
+left=<-
+right=->
+press=enter
+```
+ For keys programming use KEYS section. Example:
+```
+[KEYS]
+1=a
+2=s+b
+3=a+s+c,d,e
+```
+Available knobs and keys depends of special purposes keyboard model.
+
+See layout file complete example: [layout.ini](/layout.ini).
+
+# Programming
+Be aware that USB subsystem access needs relevant rights. `sudo` for Kubuntu.
+
+## Install Python requirements before usage:
+```
+pip install -r requirements.txt
+```
+
+## Command line help:
+```
+python3 ./keyboard_program.py -h
+usage: keyboard_program.py [-h] [-l FILEPATH] [-d] [-v]
+
+Programming utility of special purposes keyboard with encoders and keys.
+
+options:
+  -h, --help            show this help message and exit
+  -l FILEPATH, --layout FILEPATH
+                        keyboard layout file path; default: layout.ini
+  -d, --dry             don't iteract with keyboard. Set this option to validate keyboard layout file
+  -v, --verbose, -v     verbose level; use multiple times to increase log level; example: -vv
+
+Special keys: CTRL, SHIFT, ALT, META or WIN. Example: "ctrl+shift+alt+meta+<-" or "c+s+a+m+<-". Example multi-key sequence: "a,b,c" or "c+s+a+m+a,b,c". Known special keyboard USB Vendor:Product: 1189:8890. Known symbols: esc, f1, f2,
+f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, prtscn, scrolllock, pause, tilda, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -, minus, +, plus, backspace, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, [_], tab, capslock,
+enter, [{, ]}, \|, ;:, '", ,<, .>, /?, ins, del, home, end, pgup, pgdn, ->, <-, arrowdn, arrowup, numlock, numdiv, nummul, num-, num+, num., menu, num1, num2, num3, num4, num5, num6, num7, num8, num9, num0, mouseleft, mouseright,
+mousemiddle, mouseweelup, mouseweeldn
+```
+## Check layout file layout.ini:
+```
+sudo python3 ./keyboard_program.py -l "layout.ini" -vd
+DRY RUN !
+Read layout config file "layout.ini"
+Encoder 1 left  programming to "<-"
+Encoder 1 right programming to "->"
+Encoder 1 press programming to "enter"
+Key 03 programming to "3"
+Key 02 programming to "2"
+Key 01 programming to "1"
+Key 06 programming to "6"
+Key 05 programming to "5"
+Key 04 programming to "4"
+DONE
+```
+
+## Command line programming example with verbose output (use default layout file name layout.ini):
+```
+sudo python3 ./keyboard_program.py -v
+Read layout config file "layout.ini"
+SPECIAL KEYBOARD CONNECTED
+Encoder 1 left  programming to "<-"
+Encoder 1 right programming to "->"
+Encoder 1 press programming to "enter"
+Key 03 programming to "3"
+Key 02 programming to "2"
+Key 01 programming to "1"
+Key 06 programming to "6"
+Key 05 programming to "5"
+Key 04 programming to "4"
+DONE
+```
+
+## Command line programming example with more verbose output (use default layout file name layout.ini):
+```
+sudo python3 ./keyboard_program.py -vv
+Read layout config file "layout.ini"
+SPECIAL KEYBOARD CONNECTED
+Encoder 1 left  programming to "<-"
+Encoder 1 left  programming
+        Bind KEY13 to codes=('0x50',) mod=none
+Encoder 1 right programming to "->"
+Encoder 1 right programming
+        Bind KEY15 to codes=('0x4F',) mod=none
+Encoder 1 press programming to "enter"
+Encoder 1 press programming
+        Bind KEY14 to codes=('0x28',) mod=none
+Key 03 programming to "3"
+Key 03 programming to code=0x20 mod=none
+        Bind KEY03 to codes=('0x20',) mod=none
+Key 02 programming to "2"
+Key 02 programming to code=0x1F mod=none
+        Bind KEY02 to codes=('0x1F',) mod=none
+Key 01 programming to "1"
+Key 01 programming to code=0x1E mod=none
+        Bind KEY01 to codes=('0x1E',) mod=none
+Key 06 programming to "6"
+Key 06 programming to code=0x23 mod=none
+        Bind KEY06 to codes=('0x23',) mod=none
+Key 05 programming to "5"
+Key 05 programming to code=0x22 mod=none
+        Bind KEY05 to codes=('0x22',) mod=none
+Key 04 programming to "4"
+Key 04 programming to code=0x21 mod=none
+        Bind KEY04 to codes=('0x21',) mod=none
+DONE
+```
---
+++ keyboard_program.py
@@ -0,0 +1,358 @@
+#!/usr/bin/env python 
+
+from sys import argv, exit
+from time import sleep
+from usb.core import find, USBError
+from configparser import ConfigParser
+from re import match, compile, IGNORECASE
+from argparse import ArgumentParser
+
+
+DEFAULT_LAYOUT_FILEPATH = 'layout.ini'
+
+# USB parameters
+VENDOR, PRODUCT = 0x1189, 0x8890
+
+# Programming sequences
+START = '03000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000'
+PREF = '03a10100 00000000 00000000 00000000 00000000 00000000 00000000 00000000'
+# KEYkmsc: key number, keys count, key sequence index, special keys (1 CTRL, 2 SHIFT, 4 ALT, 8 META/WIN), code
+KEYkcisc = '03{:02X}11{:02X} {:02X}{:02X}{:02X}00 00000000 00000000 00000000 00000000 00000000 00000000'
+# MOUSEkbws: key number, mouse button (1 LEFT, 2 RIGHT, 4 MIDDLE), mouse weel (1 UP, 0xFF DOWN), special keys (1 CTRL, 2 SHIFT, 4 ALT)
+MOUSEkbws = '03{:02X}13{:02X} 0000{:02X}{:02X} 00000000 00000000 00000000 00000000 00000000 00000000'
+POST = '03aaaa00 00000000 00000000 00000000 00000000 00000000 00000000 00000000'
+
+# Keyboard parameters
+
+KEYS_MAX_COUNT = 6
+
+# Encoders K1, K2,.. key codes: left, right, press
+ENCODERS_KEYS = (
+	(
+		0x0D,
+		0x0F,
+		0x0E,
+	),
+)
+
+ENCODERS_KINDS = ("left ", "right", "press")
+ENCODERS_KINDS_MAX_LEN = max(len(x) for x in ENCODERS_KINDS)
+
+# Symbols codes: keyboard key symbol to code
+SYMBOLS_CODES = {
+	# first row with F keys
+	'esc': 0x29,
+	'f1': 0x3A,
+	'f2': 0x3B,
+	'f3': 0x3C,
+	'f4': 0x3D,
+	'f5': 0x3E,
+	'f6': 0x3F,
+	'f7': 0x40,
+	'f8': 0x41,
+	'f9': 0x42,
+	'f10': 0x43,
+	'f11': 0x44,
+	'f12': 0x45,
+	'prtscn': 0x47,
+	'scrolllock': 0x47,
+	'pause': 0x48,
+	# second row with numbers
+	'tilda': 0x35,
+	'1': 0x1E,
+	'2': 0x1F,
+	'3': 0x20,
+	'4': 0x21,
+	'5': 0x22,
+	'6': 0x23,
+	'7': 0x24,
+	'8': 0x25,
+	'9': 0x26,
+	'0': 0x27,
+	'-': 0x2D,
+	'minus': 0x2D,
+	'+': 0x2E,
+	'plus': 0x2E,
+	'backspace': 0x2A,
+	# letter keys
+	'a': 0x04,
+	'b': 0x05,
+	'c': 0x06,
+	'd': 0x07,
+	'e': 0x08,
+	'f': 0x09,
+	'g': 0x0A,
+	'h': 0x0B,
+	'i': 0x0C,
+	'j': 0x0D,
+	'k': 0x0E,
+	'l': 0x0F,
+	'm': 0x10,
+	'n': 0x11,
+	'o': 0x12,
+	'p': 0x13,
+	'q': 0x14,
+	'r': 0x15,
+	's': 0x16,
+	't': 0x17,
+	'u': 0x18,
+	'v': 0x19,
+	'w': 0x1A,
+	'x': 0x1B,
+	'y': 0x1C,
+	'z': 0x1D,
+	' ': 0x2C,
+	# control keys of letter block
+	'tab': 0x2B,
+	'capslock': 0x39,
+	'enter': 0x28,
+	# other keys of letter block: 3+2+3
+	'[{': 0x2F,
+	']}': 0x30,
+	'\|': 0x31,
+	';:': 0x33,
+	'\'"': 0x34,
+	',<': 0x36,
+	'.>': 0x37,
+	'/?': 0x38,
+	# edit & navigation keys block
+	'ins': 0x49,
+	'del': 0x4C,
+	'home': 0x4A,
+	'end': 0x4D,
+	'pgup': 0x4B,
+	'pgdn': 0x4E,
+	# arrows block
+	'->': 0x4F,
+	'<-': 0x50,
+	'arrowdn': 0x51,
+	'arrowup': 0x52,
+	# numpad block
+	'numlock': 0x53,
+	'numdiv': 0x54,
+	'nummul': 0x55,
+	'num-': 0x56,
+	'num+': 0x57,
+	'num.': 0x63,
+	'menu': 0x65,
+	'num1': 0x59,
+	'num2': 0x5A,
+	'num3': 0x5B,
+	'num4': 0x5C,
+	'num5': 0x5D,
+	'num6': 0x5E,
+	'num7': 0x5F,
+	'num8': 0x60,
+	'num9': 0x61,
+	'num0': 0x62,
+	# mouse
+	'mouseleft': 0x01,
+	'mouseright': 0x02,
+	'mousemiddle': 0x04,
+	'mouseweelup': 0x01,
+	'mouseweeldn': 0xFF,
+}
+
+def get_symbol_code(symbol: str | None) -> int | None:
+	'Gets code from keyboard symbol'
+	if symbol is not None and (code := SYMBOLS_CODES.get(symbol.lower() if symbol else ' ')):
+		return code
+	print(f'Unknow code for symbol "{symbol}", skip this programming')
+	return None
+
+def get_codes_and_special_keys(value: str) -> tuple[int | list[int], int] | None:
+	'return ([code1, code2,..], special_keys) or None'
+
+	def parse_special_key(value: str) -> int:
+		match value.lower():
+			case 'ctrl' | 'c': return 1
+			case 'shift' | 's': return 2
+			case 'alt' | 'a': return 4
+			case 'meta' | 'win' | 'm' | 'w': return 8
+			case _: return 0
+
+	codes, special_keys_value = [], 0
+
+	if (sequence := value.split(',')) and sequence[0]:
+		if (special_keys := sequence[0].split('+')):
+			if len(special_keys) > 2 and (not special_keys[-1] and not special_keys[-2]):  # spetial case: +
+				special_keys[-2:] = '+'
+			for special_key in special_keys[:-1]:
+				if (buff := parse_special_key(special_key)):
+					special_keys_value |= buff  # use bitwise operator to fix the multiple same special keys
+				else:
+					print(f'Can\'t parse special key: {special_key}')
+					return None
+			if (code := get_symbol_code(special_keys[-1])):
+				codes.append(code)
+			sequence = sequence[1:]
+
+	for symbol in sequence:
+		if symbol:
+			if (code := get_symbol_code(symbol)):
+				codes.append(code)
+
+	return codes if len(codes) > 1 else codes[0], special_keys_value
+
+def dump_special_keys(special_keys: int) -> str:
+	ret = ''
+	if special_keys & 1:
+		ret += 'CTRL+'
+	if special_keys & 2:
+		ret += 'SHIFT+'
+	if special_keys & 4:
+		ret += 'ALT+'
+	if special_keys & 8:
+		ret += 'META+'
+	return ret[:-1] if ret else 'none'
+
+
+def bind_key_to_symbol_code(key: int, symbol_code: int | list[int], special_keys: int = 0):
+	'''
+	key          -:- 1..KEYS_MAX_COUNT
+	symbol_code  -:- code or sequence: [code1, code2,..]
+	special keys -:- 1 CTRL, 2 SHIFT, 4 ALT, 8 META/WIN
+	'''
+	if type(symbol_code) is int:
+		symbol_code = (symbol_code,)
+	key, special_keys = key & 0xFF, special_keys & 0xFF
+	if verbose > 1:
+		print(f'	Bind KEY{key:02} to codes={tuple(f"0x{x:02X}" for x in symbol_code)} mod={dump_special_keys(special_keys)}')
+	if not dry:
+		ep.write(bytearray.fromhex(PREF))
+		ep.write(bytearray.fromhex(KEYkcisc.format(key, len(symbol_code), 0, special_keys, 0)))
+		for i, code in enumerate(symbol_code):  # keys count
+			ep.write(bytearray.fromhex(KEYkcisc.format(key, len(symbol_code), i + 1, special_keys, code & 0xFF)))
+		ep.write(bytearray.fromhex(POST))
+
+def bind_key_to_mouse(key: int, button: int, weel: int, special_keys: int = 0):
+	'''
+	key          -:- 1..KEYS_MAX_COUNT
+	button       -:- 1 LEFT, 2 RIGHT, 4 MIDDLE
+	weel         -:- 1 UP, 0xFF DOWN
+	special keys -:- 1 CTRL, 2 SHIFT, 4 ALT, 8 META/WIN
+	'''
+	key, button, special_keys = key & 0xFF, button & 0xFF, special_keys & 0xFF
+	if verbose > 1:
+		print(f'	Bind KEY{key=} to mouse {button=} {weel=:02X} mod={special_keys=}')
+	if not dry:
+		ep.write(bytearray.fromhex(PREF))
+		ep.write(bytearray.fromhex(MOUSEkbws.format(key, button, weel, special_keys)))
+		ep.write(bytearray.fromhex(POST))
+
+def bind_key(key: int, symbol_code: int | list[int], special_keys: int = 0):
+	'''
+	key          -:- 1..KEYS_MAX_COUNT
+	symbol_code  -:- code or sequence: [code1, code2,..]
+	special keys -:- 1 CTRL, 2 SHIFT, 4 ALT, 8 META/WIN
+	'''
+	if verbose > 1:
+		if type(symbol_code) == int:
+			print(f'Key {key:02} programming to code=0x{symbol_code:02X} mod={dump_special_keys(special_keys)}')
+		else:
+			print(f'Key {key:02} programming to codes={tuple(f"0x{x:02X}" for x in symbol_code)} mod={dump_special_keys(special_keys)}')
+	if 0 < key <= KEYS_MAX_COUNT:
+		bind_key_to_symbol_code(key, symbol_code, special_keys)
+	else:
+		print(f'Skip key programming. Key number out of range 1..{KEYS_MAX_COUNT}: {key}')
+
+def bind_encoder(number: int, kind: int, symbol_code: int | list[int], special_keys: int = 0):
+	if verbose > 1:
+		print(f'Encoder {number} {ENCODERS_KINDS[kind]} programming')
+	if 0 < number <= len(ENCODERS_KEYS):
+		bind_key_to_symbol_code(ENCODERS_KEYS[number - 1][kind], symbol_code, special_keys)
+	else:
+		print(f'Skip encoder programming. Encoder number out of range 1..{len(ENCODERS_KEYS)}: {number}')
+
+def bind_encoder_config_section(section: object, number: int):
+
+	def get_section_symbol_code(section: object, section_key: str) -> tuple[int | list[int], int] | None:
+		'Gets code from section & key of config file'
+		if (key_value := section.get(section_key)):
+			if verbose:
+				print(f'Encoder {number} {section_key:{ENCODERS_KINDS_MAX_LEN}} programming to "{key_value}"')
+			return get_codes_and_special_keys(key_value)
+		return None
+
+	if (codes_and_special_keys := get_section_symbol_code(section, 'left')):
+		bind_encoder(number, 0, codes_and_special_keys[0], codes_and_special_keys[1])
+	if (codes_and_special_keys := get_section_symbol_code(section, 'right')):
+		bind_encoder(number, 1, codes_and_special_keys[0], codes_and_special_keys[1])
+	if (codes_and_special_keys := get_section_symbol_code(section, 'press')):
+		bind_encoder(number, 2, codes_and_special_keys[0], codes_and_special_keys[1])
+
+
+# process command-line
+
+def parse_args() -> object:
+	parser = ArgumentParser(
+		description='Programming utility of special purposes keyboard with encoders and keys.'
+		, epilog=f'Special keys: CTRL, SHIFT, ALT, META or WIN. Example: "ctrl+shift+alt+meta+<-" or "c+s+a+m+<-". Example multi-key sequence: "a,b,c" or "c+s+a+m+a,b,c". Known special keyboard USB Vendor:Product: {VENDOR:02X}:{PRODUCT:02X}. Known symbols:\n{", ".join("{}".format(s if s.strip() else "[_]") for s in SYMBOLS_CODES)}')
+	parser.add_argument('-l', '--layout', metavar='FILEPATH', default=DEFAULT_LAYOUT_FILEPATH, help=f'keyboard layout file path; default: {DEFAULT_LAYOUT_FILEPATH}')
+	parser.add_argument('-d', '--dry', action='store_true', help=f'don\'t iteract with keyboard. Set this option to validate keyboard layout file')
+	parser.add_argument('-v', '--verbose', '-v', action='count', default=0, help='verbose level; use multiple times to increase log level; example: -vv')
+	# parser.epilog = f'Known USB Vendor:Product: {VENDOR:02X}:{PRODUCT:02X}. Known symbols:\n{", ".join("{}".format(s if s.strip() else "[_]") for s in SYMBOLS_CODES)}'
+	return parser.parse_args()
+
+args = parse_args()
+layout_file_path, dry, verbose = args.layout, args.dry, args.verbose
+if dry:
+	print('DRY RUN !')
+
+# read & parse layout config file
+if verbose:
+	print(f'Read layout config file "{layout_file_path}"')
+config = ConfigParser()
+try:
+	if not config.read(layout_file_path):
+		raise Exception('File not found')
+except Exception as e:
+	print(f'Error while read config file:\n{e}')
+	exit(1)
+
+if not dry:
+	# wait for keybord attach
+	while not (dev := find(idVendor=VENDOR, idProduct=PRODUCT)):
+		sleep(1)
+
+	if verbose:
+		print('SPECIAL KEYBOARD CONNECTED')
+
+	# get output USB endpoint
+	cfg = dev.get_active_configuration()
+	# print(f'{cfg=}')
+	i = cfg.interfaces()[1]
+	# print(f'{i=}')
+	ep = i.endpoints()[0]
+	# print(f'{ep=}')
+
+	# prepare keyboard to programming & check USB connection
+	try:
+		ep.write(bytearray.fromhex(START))
+	except USBError:
+		print('Keyboard busy, abort')
+		exit(2)
+
+# programming encoders & keys
+re_encoder, re_keys = compile('^K(\d)$', IGNORECASE), compile('^KEYS$', IGNORECASE)
+for section in config:
+	if (m := re_encoder.match(section)):
+		# encoder section # get number of encoder and symbol codes to bind
+		number, section = int(m[1]), config[section]
+		bind_encoder_config_section(section, number)
+	elif (m := re_keys.match(section)):
+		# key section # get symbol codes to bind to key number
+		section = config[section]
+		for number, symbol in section.items():
+			try:
+				number = int(number)
+			except ValueError:
+				print(f'Wrong key number "{number}", skip this programming')
+			else:
+				if (codes_and_special_keys := get_codes_and_special_keys(symbol)):
+					if verbose:
+						print(f'Key {number:02} programming to "{symbol}"')
+					bind_key(number, codes_and_special_keys[0], codes_and_special_keys[1])
+
+if verbose:
+	print('DONE')
